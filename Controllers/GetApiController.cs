204e9a37024667c9333c9dd9c8a016c3df233d77
David Oduntan <davidoduntan1@gmail.com>
2024-07-05 17:27:23 +0100

    frist commit

---
+++ Controllers/GetApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_HNG_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetApiController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetApi([FromQuery] string name)
+        {
+            var clientIp =  "127.0.0.1";
+            var location = "New York";
+            var temperature = "Hello, Mark!, the temperature is 11 degrees Celcius in New York";
+
+            var Response = new
+            {
+                clientIp = clientIp,
+                location = location,
+                greeting = $"Hello,{name}!,the {temperature} is 11 degrees Celcius in New York",
+            };
+
+            return Ok(Response);
+        }
+    }
+}
---
+++ Entities/GetApi.cs
@@ -0,0 +1,10 @@
+﻿namespace API_HNG_.Entities
+{
+    public class GetApi
+    {
+        public string ClientIp { get; set; } = default!;
+        public string Location { get; set; } = default!;
+
+        public string Greeting { get; set; } = default!;
+    }
+}
