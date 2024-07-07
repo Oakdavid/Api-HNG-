b27b81c7bfd4f63c37ecbbcdbdd6f26f0ec149e1
David Oduntan <davidoduntan1@gmail.com>
2024-07-07 17:11:47 +0100

    second commit

--- Controllers/GetApiController.cs
+++ Controllers/GetApiController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
 
 namespace API_HNG_.Controllers
 {
@@ -8,21 +12,58 @@ namespace API_HNG_.Controllers
     public class GetApiController : ControllerBase
     {
 
-        [HttpGet]
-        public IActionResult GetApi([FromQuery] string name)
-        {
-            var clientIp =  "127.0.0.1";
-            var location = "New York";
-            var temperature = "Hello, Mark!, the temperature is 11 degrees Celcius in New York";
+        private readonly IHttpClientFactory _httpClientFactory;
 
-            var Response = new
+        public GetApiController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetApi([FromQuery] string name)
+        {
+            var clientIp = HttpContext.Connection.RemoteIpAddress?.ToString();
+            double temperature = 0;
+            string city = "" ;
+
+            if (!string.IsNullOrEmpty(clientIp))
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                var clientIpData = await httpClient.GetStringAsync($"http://ip-api.com/json/{clientIp}");
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                };
+                var details = JsonSerializer.Deserialize<JsonElement>(clientIpData, options);
+
+                if (details.TryGetProperty("city", out JsonElement cityElement))
+                {
+                    city = cityElement.GetString();
+                }
+
+                if (!string.IsNullOrEmpty(city))
+                {
+                    var apiKey = "d45c77922473f77e95e106788ee2bb9f";
+                    var weatherResponse = await httpClient.GetStringAsync($"https://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}");
+                    var weatherData = JsonSerializer.Deserialize<JsonElement>(weatherResponse, options);
+
+                    if (weatherData.TryGetProperty("current", out JsonElement currentElement) &&
+                        currentElement.TryGetProperty("temp_c", out JsonElement tempElement))
+                    {
+                        temperature = tempElement.GetDouble();
+                    }
+                }
+            }
+
+            var response = new
             {
                 clientIp = clientIp,
-                location = location,
-                greeting = $"Hello,{name}!,the {temperature} is 11 degrees Celcius in New York",
+                location = city,
+                greeting = $"Hello, {name}! The temperature is {temperature} degrees Celsius in {city}.",
             };
 
-            return Ok(Response);
+            return Ok(response);
         }
+
     }
 }
--- obj/Debug/net8.0/API(HNG).AssemblyInfo.cs
+++ obj/Debug/net8.0/API(HNG).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API(HNG)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204e9a37024667c9333c9dd9c8a016c3df233d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("API(HNG)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API(HNG)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
